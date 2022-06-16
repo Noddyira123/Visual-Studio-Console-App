1c2fdb8125097ec759a5a32a042109286f606f71
Noddyira123 <96566372+Noddyira123@users.noreply.github.com>
2022-06-16 17:58:22 +1000

    Add files via upload
    enjoy!!!

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log ("My First Console Project")
+var firstname = "Eira";
+var lastname = "Krishnan";
+var fullname = firstname.concat(lastname);
+console.log (fullname);
+var a = 10;
+var b = 5;
+var add = a+b;
+console.log (add);
+var c = 10;
+var d = 5;
+var subtract = c-d;
+console.log (subtract);
+var multiply = a*b;
+console.log (multiply);
+var divide = a/b;
+console.log (divide);
